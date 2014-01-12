0cc8f756c3e65a6c8a963ddfeb0f7e2eda0e9cf4
diccfish <diccfish@gmail.com>
2014-01-12 13:06:20 -0800

    this is the flamingo story

---
+++ header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <meta name="description" content="Are there flamingos in florida? No, probably not, but there is a lot more to it. Find out why the flamingo is a symbol of florida, even though no flamingos live in florida.">
+	  
+	<title>Flamingo</title>
+
+	<link href='http://fonts.googleapis.com/css?family=Raleway:300' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="all">
+</head>
+
+<body>
---
+++ index.php
@@ -0,0 +1,123 @@
+<?php include('header.php'); ?>
+	<main>
+	
+		<article class="flamingo">
+			<section>
+				<h1>Flamingo</h1>
+			</section>
+		</article>
+
+		<article class="shout">
+			<section>
+				<h1>Florida's Most Famous Bird</h1>
+				<p>If i say flamingo, you say florida!</p>
+				<h3 class="shout1">FLAMINGO</h3>
+				<h3 class="shout2">FLORIDA</h3>
+			</section>
+		</article>
+
+		<article class="intro">
+			<section>
+				<h1>Flamingoes Are All Over Florida</h1>
+				<p>Flamingoes are on our yards, our lottery, park names, city names, our zoos...</p>
+				<h2>but they are not in our ecosystem</h2>
+				<i></i><i></i><i></i><i></i><i></i>
+				<br />
+				<i></i><i></i><i></i><i></i><i></i>
+			</section>
+		</article>
+
+		<article>
+			<section>
+				<h1>What'd I Say?</h1>
+				<p>Any flamingoes that might have been in south Florida were made into fancy flamingo hats.</p>
+				<div class="hat"></div>
+			</section>
+		</article>
+
+		<article class="audubon">
+			<section>
+				<h1>Audubon</h1>
+				<p>The badass who wrote <em>The Birds of America</em> saw a flamingo in Florida!</p>
+				<p>"On the 7th of May, 1832, while sailing from Indian Key, I for the first time saw a flock of Flamingoes. Ah, reader could you but know the emotions that then agitated my breast!"</p>
+				<p>after trying many times to capture a flamingo, he returns to London. while in london, audubon implored friends to send him a flamingo. after six years he received one from cuba. he painted the bird for his studies.</p>
+				<?php include('img/flamingo.svg'); ?>
+			</section>
+		</article>
+
+		<article>
+			<section>
+				<h1>Miami</h1>
+				<p>1920s - Miami Beach was building its first grand hotel. The hotel needed a tropical name to attract tourist, it was named The Flamingo.</p>
+				<p>1930s - Hialeah started an attraction with imported flamingoes displaying the flamingoes natural marching behavior.</p>
+				<iframe width="560" height="315" src="//www.youtube.com/embed/KW8GX2n4qbY?rel=0" frameborder="0" allowfullscreen></iframe>
+			</section>
+		</article>
+
+		<article>
+			<section>
+				<h1>Plastic</h1>
+				<p>1957 - Donald Featherstone and Union Products of New England created the plastic pink flamingo. the pink flamingo earned Featherstone a LG Nobel Prize. Now an iconic symbol of florida, with more than 20 million sold.</p>
+				<p>Tip - real ones have Featherstone's signature on the butt.</p>
+				<div class="yard">
+					<div class="house">
+						<div class="door"></div>
+						<div class="window"></div>
+					</div>
+					<div class="yard-flamingo"></div>
+				</div>
+			</section>
+		</article>
+
+		<article class="vice">
+			<section>
+				<h1>Miami, Again</h1>
+				<p>1980s - Miami Vice's opening credits feature flamingos. Once again everyone be lovin them Florida flamingoes.</p>
+				<img src="img/flamingo-bg.jpg">
+			</section>
+		</article>
+
+		<article class="badass">
+			<section>
+				<h1>Sooooo</h1>
+				<p>2013 - I purpose that flamingoes became an icon of florida through a mix of coincidence, science, tourism, capitalism, popular culture, and being fantastic dancers.</p>
+				<?php include('img/face.svg'); ?>
+			</section>
+		</article>
+
+		<article>
+			<section class="video">
+				<h1>A Lesson From Our Teacher</h1>
+					<iframe width="560" height="315" src="//www.youtube.com/embed/Wd29XD7pO9U?rel=0" frameborder="0" allowfullscreen></iframe>
+			</section>
+		</article>
+
+		<article class="map">
+			<section>
+				<h1>Flamingo Territory</h1>
+				<?php 
+					include('media/florida.svg'); 
+				?>
+				<h1 class="areaExplained">Cuba Has Flamingoes</h1>
+			</section>
+		</article>
+
+		<article class="links">
+			<section>
+				<h1>Learn More</h1>
+				<a href="http://www.sptimes.com/2002/10/29/Floridian/Real_Florida__Pink_po.shtml" target="_blank">SP Times Real Pink</a>
+				<a href="http://www.livescience.com/27322-flamingos.html" target="_blank">Live Science</a>
+				<a href="http://thefloridaexplorer.com/why-all-the-pink-flamingos/" target="_blank">The Florida Explorer</a>
+				<a href-"http://en.wikipedia.org/wiki/John_James_Audubon" target="_blank">John James Audubon (Jean-Jacques Audubon)</a>
+				<a href="http://books.google.com/books?id=3Ag6AQAAIAAJ&pg=PA172&dq=The+Birds+of+America+flamingo&hl=en&sa=X&ei=cvuEUraIJ5HpkQfS_oHYDg&ved=0CDMQ6AEwAQ#v=onepage&q=The%20Birds%20of%20America%20flamingo&f=false" target="_blank">The Birds of America</a>
+				<a href="http://benbiondo.com/" target="_blank">The Awesome Flamingo Pattern</a>
+			</section>
+		</article>
+
+		<article class="home">
+			<section>
+				<a href="/"><h1>Back Home</h1></a>
+			</section>
+		</article>
+	</main>
+<?php include('footer.php'); ?>
